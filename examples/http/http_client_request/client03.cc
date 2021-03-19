22fac77bd009db8290ceb66318081a5757eea5b5
Anonymous275 <36374260+Anonymous-275@users.noreply.github.com>
2021-03-19 03:18:06 +0200

    Update client03.cc

--- examples/http/http_client_request/client03.cc
+++ examples/http/http_client_request/client03.cc
@@ -7,27 +7,30 @@
 
 static bool responsed = false;
 static void HandleHTTPResponse(const std::shared_ptr<evpp::httpc::Response>& response, evpp::httpc::Request* request) {
-    LOG_INFO << "http_code=" << response->http_code() << " [" << response->body().ToString() << "]";
+    std::cout << "http_code=" << response->http_code() << " [" << response->body().ToString() << "]\n";
     auto* ConnectionHeader = response->FindHeader("Connection");
     if(ConnectionHeader != nullptr){
-        LOG_INFO << "HTTP HEADER Connection=" << std::string(ConnectionHeader);
+        std::cout << "HTTP HEADER Connection=" << std::string(ConnectionHeader) << "\n";
     }
     responsed = true;
     assert(request == response->request());
     delete request; // The request MUST BE deleted in EventLoop thread.
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    google::SetStderrLogging(google::GLOG_ERROR);
+
     evpp::EventLoopThread t;
     t.Start(true);
 #if defined(EVPP_HTTP_CLIENT_SUPPORTS_SSL)
-    std::shared_ptr<evpp::httpc::ConnPool> pool(new evpp::httpc::ConnPool("www.360.cn", 443,true, evpp::Duration(10.0)));
+    std::shared_ptr<evpp::httpc::ConnPool> pool(new evpp::httpc::ConnPool("reqbin.com", 443,true, evpp::Duration(10.0)));
     evpp::httpc::SET_SSL_VERIFY_MODE(SSL_VERIFY_NONE);
 #else
     std::shared_ptr<evpp::httpc::ConnPool> pool(new evpp::httpc::ConnPool("www.360.cn", 80, evpp::Duration(2.0)));
 #endif
-    auto* r = new evpp::httpc::Request(pool.get(), t.loop(), "/robots.txt", "");
-    LOG_INFO << "Do http request";
+    auto* r = new evpp::httpc::Request(pool.get(), t.loop(), "/echo/get/json", "");
+    std::cout << "Do http request\n";
     r->Execute([&r](auto && PH1) { return HandleHTTPResponse(std::forward<decltype(PH1)>(PH1), r); });
 
     while (!responsed) {
@@ -37,6 +40,6 @@ int main() {
     pool->Clear();
     pool.reset();
     t.Stop(true);
-    LOG_INFO << "EventLoopThread stopped.";
+    std::cout << "EventLoopThread stopped.\n";
     return 0;
 }
